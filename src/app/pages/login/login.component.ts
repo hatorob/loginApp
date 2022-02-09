5f966936e48aa21012139a639a3d0e7e15c43b29
Hector Toro <alejotoro94@hotmail.com>
2022-02-09 13:48:33 -0500

    SweetAlert2 - Mostrar notificaciones al ususario

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,11 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
+
+import Swal from 'sweetalert2';  //npm install sweetalert2@v9.11.0  --- esta version fue la que me sirvio
 
 @Component({
   selector: 'app-login',
@@ -13,7 +16,8 @@ export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor( private auth: AuthService) { }
+  constructor( private auth: AuthService,
+               private router: Router ) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -21,11 +25,27 @@ export class LoginComponent implements OnInit {
 
   login( form: NgForm ) {
     if( form.invalid ) {  return;  }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.login( this.usuario )
               .subscribe( resp =>  {
                   console.log(resp);
+                  Swal.close();
+                  this.router.navigateByUrl('/home');
               }, (err) => {
                 console.log(err.error.error.message);
+                Swal.fire({
+                  allowOutsideClick: false,
+                  icon: 'error',
+                  title: 'Error al autenticar',
+                  text: err.error.error.message
+                });
               });
   }
 }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { AuthService } from 'src/app/services/auth.service';
+
+import Swal from 'sweetalert2';  //npm install sweetalert2@v9.11.0  --- esta version fue la que me sirvio
 
 @Component({
   selector: 'app-registro',
@@ -12,7 +16,8 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor( private auth: AuthService ) { }
+  constructor( private auth: AuthService,
+                private router: Router ) { }
 
   ngOnInit() { 
     this.usuario = new UsuarioModel();
@@ -21,11 +26,27 @@ export class RegistroComponent implements OnInit {
   OnSubmit( form: NgForm ) {
 
     if( form.invalid ) {  return;  }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
     this.auth.nuevoUsuario( this.usuario )
               .subscribe( resp => {
                 console.log(resp);
+                Swal.close();
+                this.router.navigateByUrl('/home');
               }, (err) => {
                 console.log(err.error.error.message);  //manejo de errores
+                Swal.fire({
+                  allowOutsideClick: false,
+                  icon: 'error',
+                  title: 'Error al autenticar',
+                  text: err.error.error.message
+                });
               });  // La primera vez puede crear un error, para esto toca reiniciar el servidor para que permita terminar de empaquetar las funciones
 
   }
